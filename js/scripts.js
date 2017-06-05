0fceda3c8a969c7a3144dd35fedec0ae06bd8d94
Austin Blain <austin.a.blain@gmail.com>
2017-06-05 15:27:16 -0700

    create Hot Water Heater Company site, only landing page

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+
+$(document).ready(function() {
+  var height = $('#nav').offset().top;
+  var backup = $('.ctaPhoneNumber').offset().top;
+  $(window).resize(function() {
+    height = $('#nav').offset().top;
+    return height;
+  });
+  $(window).resize(function() {
+    backup = $('.ctaPhoneNumber').offset().top;
+    return backup;
+  });
+  $(window).scroll(function() {
+    if (height > backup) {
+      if ($(window).scrollTop() > backup) {
+        $('#nav').addClass('navbar-fixed');
+        $('.ctaPhoneNumber').addClass('padding-top');
+      }
+      if ($(window).scrollTop() < backup) {
+        $('#nav').removeClass('navbar-fixed');
+        $('.ctaPhoneNumber').removeClass('padding-top');
+      }
+    }
+    else if (height < backup) {
+      if ($(window).scrollTop() > height) {
+        $('#nav').addClass('navbar-fixed');
+        $('.ctaPhoneNumber').addClass('padding-top');
+      }
+      if ($(window).scrollTop() < height) {
+        $('#nav').removeClass('navbar-fixed');
+        $('.ctaPhoneNumber').removeClass('padding-top');
+      }
+    }
+  });
+});
